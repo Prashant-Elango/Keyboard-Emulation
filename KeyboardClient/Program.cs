6dbd4e3e394fc1f647e3e934d667bc99e106c7c7
Prashant <ram@prashant.eu.org>
2019-10-18 00:02:02 -0800

    Add project files.

---
+++ KeyboardClient/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace KeyboardClient
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk,byte bScan,uint dwFlags,int dwExtraInfo);
+
+        static void sendKey(byte bvk,short keyFlag)
+        {
+         
+            if(keyFlag == 0x0100)
+            {
+                //Key Press
+                keybd_event(bvk, 0, 0x0001, 0);
+            }
+            else
+            {
+                //Key Release
+                keybd_event(bvk, 0, 0x0002, 0);
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            if(args.Length == 1)
+            {
+                if(args[0] == "\\h" || args[0] == "-h" || args[0] == "--h" || args[0] == "help")
+                {
+                    Console.WriteLine("Keyboard Client");
+                    Console.WriteLine("         -c           sets keyboardServer Ip address");
+                    Console.WriteLine("         -p           sets port");
+                    Console.WriteLine("Usages:");
+                    Console.WriteLine("     ex: keyboardClient.exe -c 192.168.1.10 -p 5555");
+                    Console.WriteLine("     ex: keyboardClient.exe -c 192.168.1.10");
+                }
+                Environment.Exit(0);
+            }
+            var sock = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            var serverIp = "";
+            var serverPort = 0;
+            if(args.Length == 4)
+            {
+                if(args[0] == "-c")
+                {
+                    serverIp = args[1];
+                }
+                if(args[2] == "-p")
+                {
+                    try
+                    {
+                        serverPort = int.Parse(args[3]);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid port");
+                    }
+                }
+            }
+            else if(args.Length == 2)
+            {
+                if(args[0] == "-c")
+                {
+                    serverIp = args[1];
+                    serverPort = 9022;
+                }
+                else
+                {
+                    Console.WriteLine("Use -c to specify Server IP");
+                }
+            }
+            if (serverIp != "" && serverPort != 0)
+            {
+                try
+                {
+                    sock.Connect(serverIp, serverPort);
+                    Console.WriteLine("connected");
+                    while (sock.Connected)
+                    {
+                        var buffer = new byte[7];
+                        var i = sock.Receive(buffer,7,SocketFlags.None);
+                        var key = buffer[3];
+                        var keyFlag = BitConverter.ToInt16(buffer,5);
+                        sendKey(key, keyFlag);
+                        Console.WriteLine(buffer[0] + " " + buffer[1] + " " + buffer[2] + " " + buffer[3] + " " + buffer[4] + " " + buffer[5] + " " + buffer[6] + " --> "+ keyFlag);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unable to connect to server");
+
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ KeyboardServer/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyboardServer
+{
+    class Program
+    {
+        const int WH_Keyboard_LL = 13;
+
+        static lowlevel_keyboardProc _proc = HookCallBack;
+        static IntPtr _hookId = IntPtr.Zero;
+
+        static Socket sSock;
+        static Socket clientSock;
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                if (args[0] == "\\h" || args[0] == "-h" || args[0] == "--h" || args[0] == "help")
+                {
+                    Console.WriteLine("Keyboard Server");
+                    Console.WriteLine("      -c           sets ipaddress to listen on");
+                    Console.WriteLine("      -p           sets port");
+                    Console.WriteLine("      -m           Mirror Mode, Were keyboard inputs are mirrored on both local and remote machine");
+                    Console.WriteLine("     Default port is 9022");
+                    Console.WriteLine("usage:");
+                    Console.WriteLine("         keyboardServer.exe -c 192.168.1.10");
+                    Console.WriteLine("         keyboardServerexe -c 192.168.1.10 -p 9022");
+                }
+                Environment.Exit(0);
+            }
+
+            sSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var internalIp = Dns.GetHostAddresses(Dns.GetHostName()).Where(t=> t.AddressFamily == AddressFamily.InterNetwork).ToList()[0];
+                        int port = 9022;
+            if(args.Length == 4)
+            {
+                if(args[0] == "-c")
+                {
+                    var _ip = args[1];
+                    try
+                    {
+                        internalIp = Dns.GetHostAddresses(Dns.GetHostName()).Where(t => t.AddressFamily == AddressFamily.InterNetwork && t.ToString() == _ip).ToList()[0];
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid Ipv4");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                if(args[2] == "-p")
+                {
+                    var _port = args[3];
+                    try
+                    {
+                        port = int.Parse(_port);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid port");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            else if(args.Length == 2)
+            {
+                if (args[0] == "-p")
+                {
+                    var _port = args[1];
+                    try
+                    {
+                        port = int.Parse(_port);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid port");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            Console.WriteLine($"Listening on {internalIp}:{port}");
+            var ipEndPoint = new IPEndPoint(internalIp, port);
+            sSock.Bind(ipEndPoint);
+
+            var serverThread = new Thread(async () =>
+            {
+                while (true)
+                {
+                    sSock.Listen(1);
+                    clientSock = await sSock.AcceptAsync();
+                }
+            });
+            serverThread.Start();
+            if (BlockInput(true))
+            {
+                Console.WriteLine("Keyboard input grabbed successfully");
+            }
+            else
+            {
+                Console.WriteLine("Keyboard input not grabbed");
+                Console.WriteLine("Admin access is required to lock keyboard input");
+            }
+            _hookId = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookId);
+        }
+
+        delegate IntPtr lowlevel_keyboardProc(int ncode, IntPtr wParams, IntPtr lParam);
+        static IntPtr HookCallBack(int ncode, IntPtr wParams, IntPtr lParam)
+        {
+            if (ncode >= 0)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if(clientSock!=null)
+                {
+                    if(clientSock.Connected)
+                    {
+                        try
+                        {
+                            var keyFlag = BitConverter.GetBytes((short)wParams);
+                            var buffer = new byte[] { 0, 0, 0, (byte)vkCode, 0, keyFlag[0], keyFlag[1] };
+                            clientSock.Send(buffer);
+                        }
+                        catch (Exception)
+                        {
+                            clientSock = null;
+                        }
+                        
+
+                    }
+                }
+            }
+            return CallNextHookEx(_hookId, ncode, wParams, lParam);
+        }
+        static IntPtr SetHook(lowlevel_keyboardProc proc)
+        {
+            var currProcess = Process.GetCurrentProcess();
+            var mainModule = currProcess.MainModule;
+            var moduleHandle = GetModuleHandle(mainModule.ModuleName);
+            return SetWindowsHookEx(WH_Keyboard_LL, proc, moduleHandle, 0);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool BlockInput(bool fBlockIt);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr GetModuleHandle(string moduleName);
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int hookId, lowlevel_keyboardProc lpFn, IntPtr hMod, uint threadId);
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hookId);
+
+    }
+}
